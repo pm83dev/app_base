8536e6ab574298189d7deb00df42a8425bf26a83
pm83dev <paolo.miccoli.83@gmail.com>
2026-04-21 16:10:08 +0200

    commit dbcontext

--- backend/AppBase.Api/Program.cs
+++ backend/AppBase.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using AppBase.factory;
+
 // Program.cs - Entry point for the ASP.NET Core Web API application
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,9 +9,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Registrazione dei servizi
+builder.Services.InjectApplicationServices();
+
 // Add dbContext for Entity Framework Core (assuming you have an AppDbContext defined in your project)
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Configure CORS to allow requests from the Angular frontend running on http://localhost:4200
 builder.Services.AddCors(options =>
---
+++ backend/AppBase.factory/InjectApplicationServices.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AppBase.factory;
+
+public static class InjectAppServices
+{
+    public static IServiceCollection InjectApplicationServices(this IServiceCollection self) 
+        {
+            // self.AddScoped<IAccountService, AccountService>();
+            // self.AddScoped<IDeviceDataService, DeviceDataService>();
+            // self.AddScoped<DppMachineService>();
+            // self.AddAutoMapper(typeof(MappingProfile));
+            // self.AddHostedService<MqttBackgroundService>();
+            return self;
+        
+        }
+}
