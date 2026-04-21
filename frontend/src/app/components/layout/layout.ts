907e71cec696db75ffa8ca87d7f0417be36b0584
pm83dev <paolo.miccoli.83@gmail.com>
2026-04-21 12:33:02 +0200

    commit wip UI

--- frontend/src/app/components/layout/layout.ts
+++ frontend/src/app/components/layout/layout.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
 import { RouterOutlet } from '@angular/router';
+import { Header } from '../header/header';
 import { Home } from '../home/home';
 import { Sidebar } from '../sidebar/sidebar';
 
 @Component({
   selector: 'app-layout',
-  imports: [Home, Sidebar, RouterOutlet],
+  imports: [Home, Sidebar, Header, RouterOutlet],
   templateUrl: './layout.html',
   styleUrls: ['./layout.scss'],
 })
